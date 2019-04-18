534a0b50101102e44b3e4828c702ec9a33737f23
tec1850 <tec1850@rit.edu>
2019-04-18 15:33:32 -0400

    Worked on Google Map API and Vue mainly in main.js

--- google-maps/js/main.js
+++ google-maps/js/main.js
@@ -1,62 +1,74 @@
-var infowindow;
-var map;
-
 let app = new Vue({
-	el: '#root',
-	data: {
-		newName: "",
-		title: "Guest List"
+  el: "#root",
+
+  data: {
+    map: null,
+    infoWindow: null
+  },
+  components: {
+	worldZoom:{
+
 	},
-	methods:{
-		worldZoom(){
-			map.setZoom(1);
-		},
-		defaultZoom(){
-			map.setZoom(16);
-		},
-		buildingZoom(){
-			map.setZoom(20);
-		},
-		addMarker(latitude, longitude, title) {
-			let position = {lat:latitude,lng:longitude};
-			let marker = new google.maps.Marker({position: position, map:map});
-			marker.setTitle(title);
+    Gmaps: () => {
+      return new Promise((resolve, reject) => {
+        let script = document.createElement("script");
+        script.async = true;
+        script.src =
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyBV7nXkls3Zy6AZ4MoLqvNMMZLKyp5jvLI";
+        document.head.appendChild(script);
+      });
+      initmap();
+    }
+  },
+  methods: {
+    worldZoom:function() {
+      map.setZoom(1);
+    },
+    defaultZoom:function() {
+      map.setZoom(16);
+    },
+    buildingZoom:function() {
+      map.setZoom(20);
+    },
+    addMarker:function(latitude, longitude, title) {
+      let position = { lat: latitude, lng: longitude };
+      let marker = new google.maps.Marker({
+        position: this.position,
+        map: this.map
+      });
+      marker.setTitle(this.title);
 
-			// Add a listener for the click event
-			google.maps.event.addListener(marker, 'click', function(e){
-			makeInfoWindow(this.position,this.title);
-			})
-		},
-		
-		makeInfoWindow(position,msg){
-			// Close old InfoWindow if it exists
-			if(infowindow){
-				infowindow.close();
-			}
+      // Add a listener for the click event
+      google.maps.event.addListener(marker, "click", function(e) {
+        makeInfoWindow(position, title);
+      });
+    },
+    makeInfoWindow:function(position, msg) {
+      // Close old InfoWindow if it exists
+      if (this.infowindow) {
+        this.infowindow.close();
+      }
+      // Make a new InfoWindow
+      infowindow = new google.maps.InfoWindow({
+        map: this.map,
+        position: this.position,
+        content: "<b>" + this.msg + "</b>"
+      });
+    },
+    initmap:function() {
+      let mapOptions = {
+        center: { lat: 43.083848, lng: -77.6799 },
+        zoom: 16,
+        mapTypeId: google.maps.mapTypeId.ROADmap
+      };
 
-			// Make a new InfoWindow
-			infowindow = new google.maps.InfoWindow({
-				map: map,
-				position: position,
-				content: "<b>" + msg + "</b>"
-			});
-		},
-		initMap() {
-			let mapOptions = {
-				center: {lat: 43.083848, lng: -77.6799},
-				zoom: 16,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-			map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      this.map = new google.maps.map(
+        document.getElementById("map"),
+        this.mapOptions
+      );
 
-			for(let i =0;i < coffeeShops.length;i++){
-				addMarker(coffeeShops[i].latitude, coffeeShops[i].longitude, coffeeShops[i].title);
-			}
-
-			map.mapTypeId = 'satellite';
-			map.setTilt(45);
-		}
-		
-
-	}
+      this.map.mapTypeId = "satellite";
+      this.map.setTilt(45);
+    }
+  }
 });
