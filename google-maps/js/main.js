0f74fcf998f7a5393139c27fffa27927edebd916
tec1850 <tec1850@rit.edu>
2019-05-04 13:01:11 -0400

    Merge pt.2

--- google-maps/js/main.js
+++ google-maps/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 /*
 	Make `app` a global by using `var`, now we can ...&callback=app.initMap from the link above
@@ -147,65 +146,6 @@ var app = new Vue({
 
         this.addMarker(latitude, longitude, title);
       }
-=======
-let app = new Vue({
-  el: "#root",
-  data: {
-    map: null,
-    infoWindow: null
-  },
-  methods: {
-    worldZoom() {
-      map.setZoom(1);
-    },
-    defaultZoom() {
-      map.setZoom(16);
-    },
-    buildingZoom() {
-      map.setZoom(20);
-    },
-    addMarker(latitude, longitude, title) {
-      let position = { lat: latitude, lng: longitude };
-      let marker = new google.maps.Marker({
-        position: this.position,
-        map: this.map
-      });
-      marker.setTitle(this.title);
-
-      // Add a listener for the click event
-      google.maps.event.addListener(marker, "click", function (e) {
-        makeInfoWindow(position, title);
-      });
-    },
-    makeInfoWindow(position, msg) {
-      // Close old InfoWindow if it exists
-      if (this.infowindow) {
-        this.infowindow.close();
-      }
-      // Make a new InfoWindow
-      infowindow = new google.maps.InfoWindow({
-        map: this.map,
-        position: this.position,
-        content: "<b>" + this.msg + "</b>"
-      });
-    },
-    initmap() {
-      console.log("init");
-      let mapOptions = {
-        center: { lat: 43.083848, lng: -77.6799 },
-        zoom: 16,
-        mapTypeId: google.maps.mapTypeId.ROADmap
-      };
-
-      this.map = new google.maps.map(
-        document.getElementById("map"),
-        this.mapOptions
-      );
-
-      this.map.mapTypeId = "satellite";
-      this.map.setTilt(45);
-      console.log("done");
->>>>>>> 96eb88b6318432e7fafd300acbfa309a21e9fb9c
     }
   }
 });
